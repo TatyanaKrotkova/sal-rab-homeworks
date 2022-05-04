9e9f2e189c9c0d8b90ee4511b70b6defff65bbc3
Tatyana Krotkova <tanya-mitrof@yandex.ru>
2022-05-04 13:19:01 +0400

    task 1.1. done

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,6 +3,15 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
+let message;
+message = productName;
+message = message + " ";
+message = message + "за";
+message = message + " ";
+message = message + productPrice;
+message = message + " ";
+message = message + "теперь в корзине!";
+console.log(message);
 
     // создайте переменную message
     // присвойте переменной message значение переменной productName
